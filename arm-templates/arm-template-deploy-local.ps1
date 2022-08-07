8406a85d365715af189f5d81e445f52083aa7975
Michael Henderson <mike@mohenderson.com>
2022-08-07 12:14:33 -0400

    Removed old bicle files

--- arm-templates/arm-template-deploy-local.ps1
+++
@@ -1,7 +0,0 @@
-$bicepFile = "main.bicep"
-$resourceGroupName = "mercury-school"
-$administratorLogin = "administratorLogin=mercdmin"
-$administratorLoginPassword = "administratorLoginPassword=<enter admin password>"
-
-az deployment group create -g $resourceGroupName -f $bicepFile --parameters $administratorLogin $administratorLoginPassword
-
